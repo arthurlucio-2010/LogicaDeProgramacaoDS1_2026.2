3c8d570ab4ceaf296ce26bde00d5004dd0aabec6
Artur Lucio <arthurlucio200510@gmail.com>
2026-09-14 14:54:04 -0300

    aula 01 e 02

--- 01_nocoes_e_fundamentos/olamundo.py
+++ 01_nocoes_e_fundamentos/olamundo.py
@@ -1,2 +1,6 @@
-print("arthur lucio")
-print("ola mundo!")
+
+nome = input  ("digite seu nome " )
+dia= input ("digite o dia do seu nacimento")
+mes=  input("mes do seu nacimento")
+ano =  input("digite o ano do seu nacimento")
+print("seu nome e {nome} voc~e nac")
---
+++ 02_conceitos_basicos/pratica_do_aluno_mao_na_massa.py
@@ -0,0 +1,15 @@
+# TODO: Declare as variáveis abaixo e execute a célula para testar
+nome_produto = "cafe"
+quantidade_estoque = 1000
+preco_unitario = 5.0
+disponivel_para_venda = True
+
+# TODO: Escreva os prints de inspeção aqui
+print("nome do produto:",nome_produto)
+print("quantidade do estoque:",quantidade_estoque)
+print("preco unitario:",preco_unitario)
+print("disponivel para venda:",disponivel_para_venda)
+print("str" , type(nome_produto))
+print("int" , type(quantidade_estoque))
+print("float" , type(preco_unitario))
+print("bool" , type(disponivel_para_venda))
