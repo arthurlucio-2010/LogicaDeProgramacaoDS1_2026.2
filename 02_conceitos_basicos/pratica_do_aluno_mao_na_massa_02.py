86abc3c3ef4a0ac00fee1af47eebf65d6ba0a531
Artur Lucio <arthurlucio200510@gmail.com>
2026-09-15 13:05:58 -0300

    atividade 2

--- 02_conceitos_basicos/pratica_do_aluno_mao_na_massa_02.py
+++ 02_conceitos_basicos/pratica_do_aluno_mao_na_massa_02.py
@@ -3,4 +3,6 @@
 # 2. Leia o número de pessoas (int)
 # 3. Calcule o valor por pessoa
 # 4. Imprima formatado usando f-string
-valor_da_conta= input("digite o valor da conta")
+valor_da_conta = float(input("digite o valor da conta"))
+leia_o_numero_de_pessoas =  float(input("digite o numeros de pessoas"))
+calcule_o_valor_por_pessoa = float(input("o valor que cada pessoa ira pagar"))
